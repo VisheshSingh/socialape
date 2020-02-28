fbd663f9b55df9fcf1af415ca2cc0a6770dd0e8b
Vishesh Singh <VisheshSingh16@gmail.com>
2020-02-28 17:26:48 -0600

    Express & formatting response 🙌

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db = {
+  schema: [
+    {
+      userHandle: 'testuser',
+      body: 'random sampke test body',
+      createdAt: '2020-02-28T23:22:07.045Z',
+      likesCount: 5,
+      commentsCount: 2
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
 
 admin.initializeApp();
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,15 +12,23 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+// GET ALL SCREAMS
+app.get('/screams', (req, res) => {
   admin
     .firestore()
     .collection('screams')
+    .orderBy('createdAt', 'desc')
     .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        const { body, userHandle, createdAt } = doc.data();
+        screams.push({
+          screamId: doc.id,
+          body,
+          userHandle,
+          createdAt
+        });
       });
       return res.json(screams);
     })
@@ -27,14 +37,12 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
-  if (req.method != 'POST') {
-    return res.status(400).json({ error: 'Method not allowed' });
-  }
+// CREATE SCREAM
+app.post('/scream', (req, res) => {
   let newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
   admin
     .firestore()
@@ -48,3 +56,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.log('Error getting document', err);
     });
 });
+
+exports.api = functions.https.onRequest(app);
