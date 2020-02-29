bc212f2bc94f9ed8110c0e8b46bfb5ae4d6575d0
Vishesh Singh <VisheshSingh16@gmail.com>
2020-02-29 16:38:21 -0600

    🦸‍♂️Authentication Middleware 🦸‍♀️

--- functions/index.js
+++ functions/index.js
@@ -26,6 +26,39 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
 
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer ')
+  ) {
+    idToken = req.headers.authorization.split(' ')[1];
+  } else {
+    console.error('No token found');
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error({ error: err });
+      return res.status(403).json({ error: err });
+    });
+};
+
 // GET ALL SCREAMS
 app.get('/screams', (req, res) => {
   db.collection('screams')
@@ -50,12 +83,13 @@ app.get('/screams', (req, res) => {
 });
 
 // CREATE SCREAM
-app.post('/scream', (req, res) => {
+app.post('/scream', FBAuth, (req, res) => {
   let newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
+
   db.collection('screams')
     .add(newScream)
     .then(doc => {
